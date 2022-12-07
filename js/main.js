5cf93797a37981efd6c8bc8e1eb177e04a12799f
Laura Bnhm <laura.bnhm@gmail.com>
2022-12-07 10:47:10 -0500

    [add] GSAP - presentation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
+
+//***********************//
+//****** JS PAGE ********//
+//***********************//
+
 document.querySelector('.img-logo-menu').addEventListener('click', reloadPage);
 
 function reloadPage() {
@@ -7,3 +12,17 @@ function reloadPage() {
   window.location.reload()
 }
 
+
+//***********************//
+//******** GSAP *********//
+//***********************//
+
+
+// PRESENTATION
+gsap.from('.img-illustration-laura', {opacity:0, scale:0.7, duration:1.5, ease: "power4.out", delay:0.8});
+gsap.from('.img-laura-bonhomme', {opacity:0, yPercent:10, duration:1.5, ease: "power4.out", delay:0.8});
+gsap.from('.txt-jobs', {opacity:0, yPercent:10, duration:1.5, ease: "power4.out", delay:1.2});
+gsap.from('.ligne', {opacity:0, scale:0, duration:1.5, ease: "power4.out", delay:1.6});
+gsap.from('#bouton-voir-presentation', {opacity:0, yPercent:10, duration:1.5, ease: "power4.out", delay:2});
+
+// FORMATION
