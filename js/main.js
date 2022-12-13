b9aaa590ede1570df275cf96ee0876b47b5aa8f6
Laura Bnhm <laura.bnhm@gmail.com>
2022-12-13 12:34:01 -0500

    [update] RESP menu hamburger OK

--- js/main.js
+++ js/main.js
@@ -18,16 +18,16 @@ function reloadPage() {
 function menuResponsive() {
   menu_count_click++;
   console.log(menu_count_click);
-    //menu_count_click[i].addEventListener('click', function(event) {
-      if (menu_count_click %2 == 1 ) {
-        afficheMenu();
-        console.log('afficheMenu');
-      }
 
-      else {
-        effaceMenu();
-        console.log('effaceMenu');
-      }
+  if (menu_count_click %2 == 1 ) {
+    afficheMenu();
+    console.log('afficheMenu');
+  }
+
+  else {
+    effaceMenu();
+    console.log('effaceMenu');
+  }
 };
 
 function afficheMenu() {
@@ -36,23 +36,37 @@ function afficheMenu() {
   document.querySelector('#ligne-hamburger-2').style.transform = 'translate(0px, -7px) rotate(-45deg)';
   document.querySelector('#ligne-hamburger-1').style.transition = '0.3s';
   document.querySelector('#ligne-hamburger-2').style.transition = '0.3s';
+  document.querySelector('#ligne-hamburger-3').style.transition = '0.3s';
 
-  // document.querySelector('.gabarit-int').style.filter = 'blur(10px)';
-  document.querySelector('.gabarit-int').style.position = 'fixed'; //MARCHE PAS
-
-  document.querySelector('#menu-txt-hamburger-presentation').style.display = 'block';
-  document.querySelector('#menu-txt-hamburger-formation').style.display = 'block';
-  document.querySelector('#menu-txt-hamburger-projets').style.display = 'block';
-  document.querySelector('#menu-txt-hamburger-contact').style.display = 'block';
-  document.querySelector('.gabarit').style.position = 'fixed';
+  document.querySelector('.menu-hamburger-open').style.display = 'flex';
+  document.querySelector('body').style.overflow = 'hidden';
+  
   document.querySelector('.section-presentation').style.display = 'none';
+  document.querySelector('.separation').style.display = 'none';
   document.querySelector('.section-formation').style.display = 'none';
   document.querySelector('.section-a-la-une').style.display = 'none';
   document.querySelector('.section-projets').style.display = 'none';
   document.querySelector('.section-contact').style.display = 'none';
-  document.querySelector('.separation').style.display = 'none';
-  document.querySelector('.section-presentation').style.display = 'none';
-  document.querySelector('body').style.overflow = 'hidden';
+}
+
+function effaceMenu() {
+  document.querySelector('#ligne-hamburger-3').style.opacity = 1;
+  document.querySelector('#ligne-hamburger-1').style.transform = 'translate(0px, 0px) rotate(0deg)';
+  document.querySelector('#ligne-hamburger-2').style.transform = 'translate(0px, 0px) rotate(0deg)';
+  document.querySelector('#ligne-hamburger-1').style.transition = '0.3s';
+  document.querySelector('#ligne-hamburger-2').style.transition = '0.3s';
+  document.querySelector('#ligne-hamburger-3').style.transition = '0.3s';
+
+  document.querySelector('.menu-hamburger-open').style.display = 'none';
+  document.querySelector('body').style.overflow = 'scroll';
+  document.querySelector('body').style.overflowX = 'hidden';
+
+  document.querySelector('.section-presentation').style.display = 'flex';
+  document.querySelector('.separation').style.display = 'flex';
+  document.querySelector('.section-formation').style.display = 'flex';
+  document.querySelector('.section-a-la-une').style.display = 'flex';
+  document.querySelector('.section-projets').style.display = 'flex';
+  document.querySelector('.section-contact').style.display = 'flex';
 }
 
 
